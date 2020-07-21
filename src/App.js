718f129abdd5a6639e7e3f9c25f631b7de18b28f
omarcv07 <omarenriquect@gmail.com>
2020-07-20 21:52:10 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Prove
         </a>
       </header>
     </div>
